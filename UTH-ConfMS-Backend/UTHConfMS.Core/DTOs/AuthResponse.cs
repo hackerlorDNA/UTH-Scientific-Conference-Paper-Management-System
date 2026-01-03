8fa2c95ff81ddb43cc308957e679616b4f247ec3
Phungphung123 <phungnguyennhp123@gmail.com>
2026-01-03 15:11:07 +0700

    Remove File Trash

--- UTH-ConfMS-Backend/UTHConfMS.Core/DTOs/AuthResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace UTHConfMS.Core.DTOs
-{
-    public class AuthResponse
-    {
-        public int UserId { get; set; }
-        public string Email { get; set; } = null!;
-        public string Token { get; set; } = null!;
-    }
-}
--- UTH-ConfMS-Backend/UTHConfMS.Core/DTOs/AuthorDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace UTHConfMS.Core.DTOs
-{
-	public class AuthorDTO
-	{
-		public string FullName { get; set; } = string.Empty;
-		public string Email { get; set; } = string.Empty;
-		public string Affiliation { get; set; } = string.Empty;
-	}
-}
